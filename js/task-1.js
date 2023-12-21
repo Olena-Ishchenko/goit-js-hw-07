a76c005e98124454f261e193048cfdaaac7ecc78
Olena-Ishchenko <elena.dubovalova@gmail.com>
2023-12-21 23:27:32 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,6 @@
-function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const allItem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${allItem.length}`);
+allItem.forEach(item => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});

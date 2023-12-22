4108331a9e064f20211c8f097f24c71854922eb2
Olena-Ishchenko <elena.dubovalova@gmail.com>
2023-12-22 19:54:28 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+btn.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  const color = document.querySelector('.color');
+  color.textContent = getRandomHexColor();
+});

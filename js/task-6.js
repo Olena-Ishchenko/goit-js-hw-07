1f1a20aac321152de99dfe063b81946aebab4d9c
Olena-Ishchenko <elena.dubovalova@gmail.com>
2023-12-25 19:06:12 +0200

    finish

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('input');
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+create.addEventListener('click', event => {
+  if (input.value >= 1 && input.value <= 100) {
+    destroyBoxes();
+    createBoxes(input.value);
+    input.value = '';
+  }
+});
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 1; i <= amount; i++) {
+    const divBox = document.createElement('div');
+    divBox.classList.add('box');
+    divBox.style.backgroundColor = getRandomHexColor();
+    divBox.style.width = `${size}px`;
+    divBox.style.height = divBox.style.width;
+    boxes.append(divBox);
+    size += 10;
+  }
+}
+destroy.addEventListener('click', event => {
+  destroyBoxes();
+});
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
